da4ae45264205c53b18f35f22c0be4d205208f1c
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-09-29 23:16:24 -0700

    Readability changes - changed the function and variable names names `irsa_key` to `military_alphabet_key` - added the `codex` parameter to `letter_to_code` - within `letter_to_code`, I changed `military_code` to `coded_letter` - within `word_to_code`, I added the `codex` argument to `letter_to_code` function call - added the `x` argument to the function call `letter_to_code` within the main function

--- irds.py
+++ irds.py
@@ -3,29 +3,29 @@
 
 import string
 
-def irsa_key() -> dict:
+def military_alphabet_key() -> dict:
     code = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 
         'Golf', 'Hotel', 'India', 'Juliet', 'Kilo', 'Lima', 
         'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 
         'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-Ray', 'Yankee', 'Zulu']
     alphabet = string.ascii_uppercase
-    irsa_key = {key:value for key, value in zip(alphabet, code)}
-    return irsa_key
+    military_alphabet_key = {key:value for key, value in zip(alphabet, code)}
+    return military_alphabet_key
 
-def letter_to_code(letter: str) -> str:
-    military_code = irsa_key()
-    return military_code[letter]
+def letter_to_code(letter: str, codex: dict) -> str:
+    coded_letter = codex[letter]
+    return coded_letter
 
 def word_to_code(word: str, codex: dict) -> str:
     coded_word = ''
     for letter in word:
-        coded_word += letter_to_code(letter.upper())
+        coded_word += letter_to_code(letter.upper(), codex)
     return coded_word
 
 if __name__ == '__main__':
-    x = irsa_key()
+    x = military_alphabet_key()
     print(x)
-    y = letter_to_code('M')
+    y = letter_to_code('M', x)
     print(y)
     z = word_to_code('Daniel', x)
     print(z)
